f2a3bea3d802540f4f94b9066c1a99f23ce48b99
kristiandiana <kristian8diana@gmail.com>
2024-07-24 17:18:38 -0400

    Updated README.md

--- api/api.py
+++ api/api.py
@@ -67,4 +67,4 @@ def receive_text():
         return jsonify({"status": "error", "message": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 8080)))
