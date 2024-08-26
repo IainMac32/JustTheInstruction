28e8cabc0040a7304e86a7debea56bfc6d552576
Iain Macdonald <65032680+IainMac32@users.noreply.github.com>
2024-08-26 13:44:30 -0400

    shows response in extension

--- content_script.js
+++ content_script.js
@@ -1,24 +1,36 @@
+// content_script.js
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.action === 'copyText') {
         const textElements = document.querySelectorAll('h1, h2, h3, h4, h5, p, li, td, caption, a'); //maybe add span back
         let puretext = "";
 
+
         for (let i = 0; i < textElements.length; i++) {
-            console.log(textElements[i].textContent); // remove to not see in console
             puretext += textElements[i].textContent + " ";  // Adding a space between elements for readability
         }
+        console.log(puretext);
 
 
         // Send puretext to Python script
-        fetch('https://api-update-v2-3gu632umxq-uc.a.run.app/receive_text', { // api endpoint updated
+        fetch('https://my-flask-app-3gu632umxq-nn.a.run.app/receive_text', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({ text: puretext })
         })
-        .then(response => response.json())
-        .then(data => console.log('Success:', data))
-        .catch(error => console.error('Error:', error));
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok ' + response.statusText);
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('Success!!:', data["text_received"]);
+           
+            // Send the received data back to the popup
+            chrome.runtime.sendMessage({ action: 'showText', text: data["text_received"] });
+        })
+        .catch(error => console.error('Error!!:', error));
     }
 });
--- popup.js
+++ popup.js
@@ -1,5 +1,26 @@
+// popup.js
 document.getElementById('circle-button').addEventListener('click', () => {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
         chrome.tabs.sendMessage(tabs[0].id, { action: 'copyText' });
     });
+});
+
+
+// Listen for messages from content_script.js
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.action === 'showText') {
+        // Update the text in the second screen
+        document.getElementById('received-text').textContent = request.text;
+       
+        // Switch to the text screen
+        document.getElementById('button-screen').style.display = 'none';
+        document.getElementById('text-screen').style.display = 'block';
+    }
+});
+
+
+// Handle the back button to go back to the first screen
+document.getElementById('back-button').addEventListener('click', () => {
+    document.getElementById('text-screen').style.display = 'none';
+    document.getElementById('button-screen').style.display = 'block';
 });
